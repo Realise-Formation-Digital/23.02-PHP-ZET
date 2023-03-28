8744857cc921f9df5b468eaa78ed83ab6d29af31
24Syleo <sylvain.beggiora@outlook.com>
2023-03-28 16:12:53 +0200

    footer.php

--- login.php
+++ login.php
@@ -1,3 +1,17 @@
 <?php require './header.php'; ?>
 
+<div class="container-md mx-auto">
+    <form action="login.php" method="post">
+        <div class="mb-3">
+            <label for="exampleFormControlInput" class="form-label">Votre nom</label>
+            <input type="text" class="form-control" id="exampleFormControlInput" placeholder="votre nom" name="name">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Votre email</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="votre email" name="email">
+        </div>
+        <button type="submit">soumettre</button>
+    </form>
+</div>
+
 <?php require './footer.php'; ?>
