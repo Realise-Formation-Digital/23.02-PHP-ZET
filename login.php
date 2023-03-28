a9102382e3a0524ff6b8f68ed6ab8ff6d44b5a0e
24Syleo <sylvain.beggiora@outlook.com>
2023-03-28 16:45:40 +0200

    vertical

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php require './header.php'; ?>
 
-<div class="container-md mx-auto">
+<div class="container-sm mx-auto my-2">
     <form action="login.php" method="post">
         <div class="mb-3">
             <label for="exampleFormControlInput" class="form-label">Votre nom</label>
             <input type="text" class="form-control" id="exampleFormControlInput" placeholder="votre nom" name="name">
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Votre email</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="votre email" name="email">
+            <label for="exampleFormControlInput1" class="form-label">Votre mot de passe</label>
+            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="votre mot de passe" name="password">
         </div>
-        <button type="submit">soumettre</button>
+        <button type="submit" class="btn btn-primary mx-auto">soumettre</button>
     </form>
 </div>
 
