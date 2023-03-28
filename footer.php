3f1c58fc4ae9183e4cc9053808bf9480a914df1e
SvetTual <125543687+SvetTual@users.noreply.github.com>
2023-03-28 16:19:49 +0200

    footer done

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer class="container-fluid footer-background-image text-dark">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2"><!-- spacer --></div>
+            <div class="col-sm-1 mt-5 mb-5 text-start">
+                <img src="img/Footer/ZET.png" alt="logo" />
+            </div>
+            <div class="col-sm-2 mt-5 mb-5 text-start">
+                <small>© Cypyright 2015. All Rights Reserved</small>
+            </div>
+            <div class="col-sm-3"><!-- spacer --></div>
+            <div class="col-sm-1 mt-5 mb-5 p-1 text-end">
+                <small>Stay In Touch</small>
+            </div>
+            <div class="col-sm-2 mt-5 mb-5 text-start">
+                <a href="www.facebook.com/ZET-services">
+                <img src="img/Footer/facebook7.png" alt="facebook" />
+                </a>
+                <a href="www.twitter.com/ZET-services">
+                <img src="img/Footer/social71.png" alt="twitter" />
+                </a>
+                <a href="www.google-plus.com/ZET-services">
+                <img src="img/Footer/social92.png" alt="google-plus" />
+                </a>
+                <a href="www.dribble.com/ZET-services">
+                <img src="img/Footer/dribble.png" alt="dribble" />
+                </a>
+            </div>
+        </div>
+      </div>
+</footer>
+    <script src="main.js" type="text/javascript"></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+      crossorigin="anonymous"
+    ></script>
+    <script src="main.js"></script>
+  </body>
+</html>
