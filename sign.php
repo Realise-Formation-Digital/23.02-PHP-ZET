72a1a4008d0522c9d12b689dd51b278c70f13f22
Amin Mamadnazarov <82723081+aminmamadnazarov@users.noreply.github.com>
2023-03-29 14:21:32 +0200

    creation logic sign

--- sign.php
+++ sign.php
@@ -1,42 +1,5 @@
 <?php
-$username = $password = $usernameError = $passwordError = $hash = $messageSucces = "";
-
-if(!empty($_POST))
-{
-    $username = $_POST['name'];
-    $password = $_POST['password'];
-    $validation = true;
-    $filename = 'user.csv';
-    $existe = 0;
-     if(empty($username))
-     {
-        $usernameError = 'mettre un nom';
-        $validation = false;
-     } 
-     if(empty($password))
-     {
-        $passwordError = 'Add your password';
-        $validation = false;
-     }
-     if($validation)
-     {
-        $hash = crypt($password, '$1$rasmusle$');
-        if(file_exists($filename))
-        {
-            $existe = 1;
-        }
-        $userCsv = fopen('user.csv', 'a');
-        if(!$existe)
-        {
-            fputcsv($userCsv,array('username;password'));
-        }
-        fputcsv($userCsv,array($username.";".$hash));
-
-        fclose($userCsv);
-
-        $messageSucces = 'Vos donnees sont bien enregistrer';
-     }
-}
+require_once 'logicSign.php';
 
 
 require 'header.php';
