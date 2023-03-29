e16384af0d910cd1f3f4c419144c226bd10ef817
24Syleo <sylvain.beggiora@outlook.com>
2023-03-29 14:42:28 +0200

    logicLogin.php

--- login.php
+++ login.php
@@ -1,40 +1,4 @@
 <?php 
-$name = $password = $passwordError = $messageError = $result = "";
-
-if(!empty($_POST))
-{
-    $name = $_POST['name'];
-    $password = $_POST['password'];
-    $success = true;
-    if(empty($name))
-    {
-        $messageError = "Vous devez rentrer un nom ou un mot de passe";
-        $success = false;
-    }
-    if(empty($password))
-    {
-        $messageError = "Vous devez rentrer un nom ou un mot de passe";
-        $success = false;
-    }
-    if($success)
-    {
-        if (($handle = fopen("user.csv", "r")) !== FALSE) {
-            while (($data = fgetcsv($handle, null, ";")) !== FALSE) 
-            {
-                if (password_verify($password, $data[1]) && $name === $data[0]) {
-                    $passwordError = "mot de passe valid $data[0]";
-                    break;
-                } 
-                else 
-                {
-                    $passwordError = 'L utilisateur ou le mot de passe est invalide.';
-                }
-            }
-            fclose($handle);
-        }
-    }
-}
-
 
 require './header.php';
 ?>
