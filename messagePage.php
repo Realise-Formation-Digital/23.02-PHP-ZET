d08a694d655c9b2c9750bcf50d2d9e349082f24e
24Syleo <sylvain.beggiora@outlook.com>
2023-03-29 16:36:32 +0200

    asdgasdrg

--- messagePage.php
+++ messagePage.php
@@ -1,13 +1,19 @@
 <?php
 session_start();
 
-$handle = fopen("message.csv", "r");
-$lignes = file("message.csv");
-var_dump($lignes);
+$file = "message.csv";
+$openfile = fopen($file,"r");
+$content = fread($openfile, filesize($file));
+var_dump($content);
+$str = explode(';', $content);
+var_dump($str);
 
 require 'header.php'; 
 ?>
-<?php foreach($lignes as $l): ?>
-    <p><?= $l ?></p>
-<?php endforeach; ?>
+<h1 class="text-center">Bonjour <?= $_SESSION['name'] ?></h1>
+<div class="container-grid-message">
+    <?php for($i=0;$i < count($str); $i++ ): ?>
+      
+    <?php endfor; ?>
+</div>
 <?php require 'footer.php'; ?>
