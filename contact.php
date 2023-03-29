aa2e2d584485e00de1106778756bd674384cbc53
Daniel Sajdak <danielsajdak65@gmail.com>
2023-03-29 22:46:26 +0200

    three different forms

--- contact.php
+++ contact.php
@@ -6,15 +6,15 @@ require_once 'logicContact.php';
  require 'header.php';
 ?>
 
-<div id="contact" class="container-fluid text-center">
+<!-- <div id="contact" class="container-fluid text-center">
       <div class="row">
         <div class="col col-12 col-xl-6 col-sm-12 map">
           <img class="pointer" src="./img/point.png" alt="pointer" />
         </div>
         <div class="col col-12 col-xl-6 col-sm-12 contact overlay">
-          <img class="overlay" src="./img/overlay.png" alt="effet" />
+          <img class="overlay" src="./img/overlay.png" alt="effet" /> -->
           <!-- Formulaire -->
-          <form class="formulaire" action="contact.php" method="post">
+          <!-- <form class="formulaire" action="contact.php" method="post">
             <div class="form-row">
               <label for="name">Name</label>
               <input type="text" id="name" name="name" />?>
@@ -40,7 +40,23 @@ require_once 'logicContact.php';
           </form>
         </div>
       </div>
-    </div>
+    </div> -->
+
+    <form class="text-center">
+  <div class="mb-3 ">
+    <input type="text" class="form-control" id="" aria-describedby="">
+  </div>
+  <div class="mb-3">
+    <input type="email" class="form-control" id="exampleInputemail1">
+  </div>
+  <div class="mb-3 ">
+    <input type="textarea" class="form-control" id="" aria-describedby="">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
 
 <?php
  require 'footer.php';
