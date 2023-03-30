f65103081cc9306ec319fd6a9fcc5bc682c78d98
24Syleo <sylvain.beggiora@outlook.com>
2023-03-30 16:28:43 +0200

    design formulaire

--- login.php
+++ login.php
@@ -11,15 +11,15 @@ require './elements/header.php';
         <?= $messageError ?>
     </div>
 <?php endif; ?>
-<div class="container-sm mx-auto my-2">
-    <form action="login.php" method="post">
+<div class="container-sm mx-auto my-2 shadow-lg p-5 rounded-5">
+    <form action="login.php" method="post" class="was-validated">
         <div class="mb-3">
             <label for="exampleFormControlInput" class="form-label">Votre nom</label>
-            <input type="text" class="form-control" id="exampleFormControlInput" placeholder="votre nom" name="name">
+            <input type="text" class="form-control is-valid" id="exampleFormControlInput" placeholder="votre nom" name="name" required>
         </div>
         <div class="mb-3">
             <label for="exampleFormControlInput1" class="form-label">Votre mot de passe</label>
-            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="votre mot de passe" name="password">
+            <input type="password" class="form-control is-valid" id="exampleFormControlInput1" placeholder="votre mot de passe" name="password" required>
         </div>
         <button type="submit" class="btn btn-primary mx-auto">soumettre</button>
     </form>
