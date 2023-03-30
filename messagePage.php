fb69f2fba26982b4e2443ffc0eb60d63abf855f8
24Syleo <sylvain.beggiora@outlook.com>
2023-03-30 16:52:37 +0200

    si pas de message traitée

--- messagePage.php
+++ messagePage.php
@@ -7,9 +7,16 @@ require './elements/header.php';
 
 ?>
 <h1 class="text-center">Bonjour <?= $_SESSION['name'] ?></h1>
+    <?php if(isset($_SESSION['notFiles'])): ?>
+        <div class="alert alert-info text-center">
+            <?= $_SESSION['notFiles'] ?>
+        </div>
+    <?php endif; ?>
     <table class="table table-striped mx-auto shadow-lg">
         <tbody>
+           <!--  boucle les lignes et stock dans la variable tab, tab est un tableau -->
            <?php foreach($lignes as $l):?>
+           <!--  explode permet de séparer les lignes via dans ce cas le ";" et crée un tableau ensuite chaques lignes est traité par une boucle -->
             <?php $tab = explode(";", $l) ?>
                     <tr>
                         <?php foreach($tab as $k => $t): ?>
