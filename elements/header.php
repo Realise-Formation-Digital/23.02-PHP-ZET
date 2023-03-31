f89d00bef8dc7344be1aae60258e951d144482e9
24Syleo <sylvain.beggiora@outlook.com>
2023-03-31 09:22:15 +0200

    files

--- elements/header.php
+++ elements/header.php
@@ -1,49 +1,23 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>ZET</title>
-    <link rel="stylesheet" href="./styles.css" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"/>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet"/>
-  </head>
-  <nav class="navbar navbar-light offset-lg-3 col-lg-2 offset-sm-0 col-sm-12 justify-content-center">
-    <button
-        class="navbar-toggler custom-toggler border-0 text-white"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbar"
-        aria-controls="navbar"
-        aria-expanded="false"
-        aria-label="Navbar navigation"> MENU 
-            <span class="text-white"><i class="bi bi-list"></i></span>
-    </button>
-        <div class="collapse" id="navbar">
-            <div class="p-4">
-                <div class="fs-4">
-                    <a href="#services" class="text-white">Services</a>
-                </div>
-                <div class="fs-4">
-                    <a href="index.php" class="text-white">Works</a>
-                </div>
-                <div class="fs-4">
-                    <a href="index.php#section-happy-clients" class="text-white">Happy Clients</a>
-                </div>
-                <div class="fs-4">
-                    <a href="index.php#professional-team" class="text-white">Our Professional Team</a>
-                </div>
-                <div class="fs-4">
-                    <a href="contact.php" class="text-white">Contact</a>
-                </div>
-                <div class="fs-4">
-                    <a href="login.php" class="text-white">Login</a>
-                </div>
-                <div class="fs-4">
-                    <a href="sign.php" class="text-white">Inscription</a>
-                </div>
-            </div>
+<?php require_once 'elements/doctyp.php'?>
+
+<body>
+    
+    <header class="container-fluid w-100 text-center align-content-center header-background-image text-light" id="home">
+      <div class="row">
+        <!-- GRID DU HEADER AVEC DES ESPACES POUR METTRE EN PAGE LE CONTENU -->
+        <div class="col-2"><!-- spacer --></div>
+        <!-- LOGO AVEC LIEN "HOME" -->
+        <div class="col-4 mt-5 mb-5 text-start">
+          <a href="#home">
+            <img src="img/Header/logo.png" alt="home-logo" />
+          </a>
         </div>
-</nav>
+        <!-- MENU AVEC LE LIEN À DEFINIR OU FENETRE A DEFINIR -->
+
+        <?php require_once 'elements/nav.php'?>
+
+     <?php  /* include_once 'textheader.php' */ ?>
+        
+    </header>
+      
+      
---
+++ elements/nav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-light offset-lg-3 col-lg-2 offset-sm-0 col-sm-12 justify-content-center">
+    <button
+        class="navbar-toggler custom-toggler border-0 text-white"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbar"
+        aria-controls="navbar"
+        aria-expanded="false"
+        aria-label="Navbar navigation"> MENU 
+            <span class="text-white"><i class="bi bi-list"></i></span>
+    </button>
+        <div class="collapse" id="navbar">
+            <div class="p-4">
+                <div class="fs-4">
+                    <a href="#services" class="text-white">Services</a>
+                </div>
+                <div class="fs-4">
+                    <a href="index.php" class="text-white">home</a>
+                </div>
+                <div class="fs-4">
+                    <a href="#section-happy-clients" class="text-white">Happy Clients</a>
+                </div>
+                <div class="fs-4">
+                    <a href="#pricing-table" class="text-white">Pricing Table</a>
+                </div>
+                <div class="fs-4">
+                    <a href="#professional-team" class="text-white">Our Professional Team</a>
+                </div>
+                <div class="fs-4">
+                    <a href="contact.php" class="text-white">Contact</a>
+                </div>
+                <div class="fs-4">
+                    <a href="login.php" class="text-white">Login</a>
+                </div>
+                <div class="fs-4">
+                    <a href="sign.php" class="text-white">Inscription</a>
+                </div>
+            </div>
+        </div>
+</nav>
