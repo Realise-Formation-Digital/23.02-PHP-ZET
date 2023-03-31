0c5d566a3b5e33fc3603d6fea2b8c3c4fdf832d9
SvetTual <125543687+SvetTual@users.noreply.github.com>
2023-03-31 12:12:06 +0200

    checkInput

---
+++ logic/checkInput.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * function pour controler les entres
+ * @param{string} -parametre vient d'inmput
+ * 
+ */
+
+function checkInput (string $value){
+$value = trim($value);
+
+$value = htmlentities($value);
+return $value;
+
+}
+
+
+?>
